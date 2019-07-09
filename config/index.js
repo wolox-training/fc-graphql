dd58e16ef966a384a3e96b8b6e50796137d198a4
Franco <franco.coronel@wolox.com.ar>
2019-07-09 12:27:24 -0500

    Add name property in database config

--- config/index.js
+++ config/index.js
@@ -30,6 +30,7 @@ const config = {
     database: {
       host: process.env.DB_HOST,
       port: process.env.DB_PORT,
+      name: process.env.DB_NAME,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD
     },
